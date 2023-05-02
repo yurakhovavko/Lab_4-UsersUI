1ede267884b472031010ca150041e602ee629fe2
krosbite <killer1233215@mail.ru>
2023-05-02 21:38:36 +0300

    basket

--- app/src/main/java/com/topic2/android/notes/theme/Theme.kt
+++ app/src/main/java/com/topic2/android/notes/theme/Theme.kt
@@ -2,11 +2,13 @@ package com.topic2.android.notes.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
 private val LightThemeColors = lightColors(
   primary = rwGreen,
@@ -14,10 +16,11 @@ private val LightThemeColors = lightColors(
   secondary = rwGreen
 )
 
-private val DarkThemeColors = lightColors(
-  primary = rwGreen,
-  primaryVariant = rwGreenDark,
-  secondary = rwGreen
+private val DarkThemeColors = darkColors(
+  primary = Color(0xFF00A055),
+  primaryVariant = Color(0xFF00F884),
+  secondary = rwGreen,
+  onPrimary = Color.White,
 )
 
 /**
